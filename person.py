d41517d2dd5236c780d1e6529993693ed0470e6f
beru26 <berkes.edina@gmail.com>
2016-06-21 15:55:42 +0200

    Class OK, but NO Error handling

--- person.py
+++ person.py
@@ -1,3 +1,9 @@
 class Person:
+
+    GENDER_TYPE = ["male", "female", "notsure"]
+
     def __init__(self, first_name, last_name, year_of_birth, gender):
-        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year_of_birth = year_of_birth
+        self.gender = gender
