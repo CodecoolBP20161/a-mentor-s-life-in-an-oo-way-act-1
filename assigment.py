2fd6da5bdfcb50ff272e4c99b4505de6870166b1
beru26 <berkes.edina@gmail.com>
2016-06-22 13:10:23 +0200

    assignment class OK

--- assigment.py
+++
@@ -1,8 +0,0 @@
-class Assigment():
-    def __init__(self, name, submit):
-        self.name = name
-        self.submit = submit
-
-    def check_assigment(self):
-        # Check submit
-        return
