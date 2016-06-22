584213adc21b36bcddaa8045aa7ce6c1e4d3bd28
beru26 <berkes.edina@gmail.com>
2016-06-22 15:02:14 +0200

    feedback class give feedback OK

--- feedback.py
+++ feedback.py
@@ -1,11 +1,12 @@
+from mentor import Mentor
+from student import Student
+
+
 class Feedback():
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, mentor_name):
+        self.mentor_name = mentor_object_name
 
-    def give_feedback(self, target):
-        # Change two object values (receiver and sender).
-        return
-
-    def get_feedback(self, target):
-        # Change two object values (receiver and sender).
-        return
+    def give_feedback(self, person_object):
+        person_object.knowledge_level += 5
+        person_object.joy += 5
+        print("{0} gave a compliment to {1}, so {1}'s knowledge and joy level increased by 5".format(self.mentor_name, person_object.name))
