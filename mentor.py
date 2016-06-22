ac4cc1572cc0c7b1478f0305f9cee81f4c71dc25
Matolay <matolaypal@gmail.com>
2016-06-22 14:45:34 +0200

    create_by_csv @staticmethod OK

--- mentor.py
+++ mentor.py
@@ -1,5 +1,20 @@
 from person import Person
+import csv
+import os
 
 
 class Mentor(Person):
-    pass
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname):
+        super().__init__(first_name, last_name, year_of_birth, gender)
+        self.nickname = nickname
+
+    @staticmethod
+    def create_by_csv():
+        list_mentorobjects = []
+        current_file_path = os.path.dirname(__file__)
+        filereader = csv.reader(open(current_file_path + "/data/mentors.csv"), delimiter=';')
+        for row in filereader:
+            mentor_firstname = str(row[0]+"_mentor")
+            mentor_firstname = Mentor(row[0], row[1], row[2], row[3], row[4])
+            list_mentorobjects.append(mentor_firstname)
+        return list_mentorobjects
