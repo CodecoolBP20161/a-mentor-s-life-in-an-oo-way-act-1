2a2c0349135413339e1e9cd8275928320598b360
beru26 <berkes.edina@gmail.com>
2016-06-22 11:26:05 +0200

    Person parent class OK

--- person.py
+++ person.py
@@ -3,32 +3,12 @@ class Person:
     GENDER_TYPE = ["male", "female", "notsure"]
 
     def __init__(self, first_name=None, last_name=None, year_of_birth=None, gender=None):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.year_of_birth = year_of_birth
-        self.gender = gender
-
-    def is_attribute(self):
         try:
-            if isinstance(first_name, Person):
-                if self.first_name == str(self.first_name):
-                    pass
-                else:
-                    raise AttributeError
-            elif isinstance(last_name, Person):
-                if self.last_name == str(self.last_name):
-                    pass
-                else:
-                    raise AttributeError
-            elif isinstance(year_of_birth, Person):
-                if self.year_of_birth == int(self.year_of_birth):
-                    pass
-                else:
-                    raise AttributeError
-            elif isinstance(gender, Person):
-                if self.last_name == str(self.gender) and self.gender in GENDER_TYPE:
-                    pass
-                else:
-                    raise AttributeError
-        except AttributeError:
+            self.first_name = first_name
+            self.last_name = last_name
+            self.year_of_birth = int(year_of_birth)
+            self.gender = gender
+        except:
             raise AttributeError
+        if self.gender not in Person.GENDER_TYPE:
+            raise AttributeError("{} is not a valid gender.".format(self.gender))
