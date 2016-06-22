e32fae833ef496cf632f5773e3008fb0d528a421
Matolay <matolaypal@gmail.com>
2016-06-22 15:29:25 +0200

    More attribute OK

--- student.py
+++ student.py
@@ -1,5 +1,23 @@
 from person import Person
+import csv
+import os
 
 
 class Student(Person):
-    pass
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname, knowledge_level, energy_level, joy_level):
+        super().__init__(first_name, last_name, year_of_birth, gender)
+        self.nickname = nickname
+        self.knowledge_level = knowledge_level
+        self.energy_level = energy_level
+        self.joy_level = joy_level
+
+    @staticmethod
+    def create_by_csv():
+        list_studentobjects = []
+        current_file_path = os.path.dirname(__file__)
+        filereader = csv.reader(open(current_file_path + "/data/students.csv"), delimiter=';')
+        for row in filereader:
+            student_firstname = str(row[0]+"_student")
+            student_firstname = Student(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+            list_studentobjects.append(student_firstname)
+        return list_studentobjects
