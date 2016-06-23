8119a7eaa15fba2357197323badf3052da1f1c81
Matolay <matolaypal@gmail.com>
2016-06-23 15:56:35 +0200

    get_feedback method OK

--- feedback.py
+++ feedback.py
@@ -3,11 +3,22 @@ from student import Student
 
 
 class Feedback():
+
     def __init__(self, mentor_name):
         self.mentor_name = mentor_name
 
     def give_feedback(self, person_object):
         person_object.knowledge_level += 5
-        person_object.joy += 5
-        print("{} gave a feedback to {}.".format(self.mentor_name, person_object.name))
-        print("{}'s knowledge and joy level increased by 5".format(person_object.name))
+        person_object.joy_level += 5
+        print(
+            "{0} gave a compliment to {1}, so {1}'s knowledge and joy level increased by 5".format(
+                self.mentor_name,
+                person_object.nickname))
+
+    @staticmethod
+    def get_feedback(mentor_object):
+        mentor_object.energy_level += 5
+        mentor_object.joy_level += 5
+        print(
+            "{0} asked the students for some feedback. {0}'s energy and joy level increased by 5".format(
+                mentor_object.nickname))
