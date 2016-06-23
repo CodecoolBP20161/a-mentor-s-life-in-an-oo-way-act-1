10994c3ddc795945513229ded79e527d3c242837
beru26 <berkes.edina@gmail.com>
2016-06-23 15:05:20 +0200

    create_full_name_list method OK

--- student.py
+++ student.py
@@ -4,7 +4,9 @@ import os
 
 
 class Student(Person):
-    def __init__(self, first_name, last_name, year_of_birth, gender, nickname, knowledge_level, energy_level, joy_level):
+
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname,
+                 knowledge_level, energy_level, joy_level):
         super().__init__(first_name, last_name, year_of_birth, gender)
         self.nickname = nickname
         self.knowledge_level = knowledge_level
@@ -27,9 +29,13 @@ class Student(Person):
         filereader = csv.reader(open(current_file_path + "/data/students.csv"), delimiter=';')
         return sum(1 for line in filereader)
 
-    def create_full_name():
+    def create_full_name_list():
+
+        full_name_list = []
+
         current_file_path = os.path.dirname(__file__)
         filereader = csv.reader(open(current_file_path + "/data/students.csv"), delimiter=';')
         for row in filereader:
             full_name = str(row[0] + " " + row[1])
-        return full_name
+            full_name_list.append(full_name)
+        return full_name_list
