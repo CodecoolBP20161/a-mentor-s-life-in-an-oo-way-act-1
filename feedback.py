665d4b12a1cc881fa93c34f14a2a020a66ec0e9a
beru26 <berkes.edina@gmail.com>
2016-06-23 10:24:24 +0200

    give_feedback print corrected

--- feedback.py
+++ feedback.py
@@ -4,9 +4,10 @@ from student import Student
 
 class Feedback():
     def __init__(self, mentor_name):
-        self.mentor_name = mentor_object_name
+        self.mentor_name = mentor_name
 
     def give_feedback(self, person_object):
         person_object.knowledge_level += 5
         person_object.joy += 5
-        print("{0} gave a compliment to {1}, so {1}'s knowledge and joy level increased by 5".format(self.mentor_name, person_object.name))
+        print("{} gave a feedback to {}.".format(self.mentor_name, person_object.name))
+        print("{}'s knowledge and joy level increased by 5".format(person_object.name))
