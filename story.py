ff3bfbd5b8c63bae8937bbe1f8a9b3f36ac2b01e
Matolay <matolaypal@gmail.com>
2016-06-23 15:38:25 +0200

    First step

--- story.py
+++ story.py
@@ -2,4 +2,8 @@ from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
 
-codecool_bp = CodecoolClass.create_local
+miki = Mentor.create_by_csv()[0]
+dani = Mentor.create_by_csv()[1]
+tomi = Mentor.create_by_csv()[2]
+
+móni = Student.create_by_csv()[0]
