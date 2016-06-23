f3d2b330b3ab936b5153f4cf19d714e3068e23bc
Matolay <matolaypal@gmail.com>
2016-06-23 11:38:11 +0200

    More function: tool_repair AND more class: Laptop, method: coding

--- electronic.py
+++ electronic.py
@@ -1,4 +1,5 @@
 class Electronic():
+
     def __init__(self, name, switch, error):
         self.name = name
         self.switch = switch
@@ -7,8 +8,31 @@ class Electronic():
     def tool_switcher(self):
         if self.error is False:
             if self.switch is True:
-                return False
+                self.switch = False
+                print("The tool is switched OFF")
             else:
-                return True
+                self.switch = True
+                print("The tool is switched ON")
         else:
-            raise ElectronicError
+            print("The tool doesn't working! :(")
+
+    def tool_repair(self):
+        if self.error is True:
+            self.error = False
+            print("The tool is working now! :)")
+        else:
+            print("The tool already working!")
+
+
+class Laptop(Electronic):
+    def __init__(self, name, switch, error):
+        super().__init__(name, switch, error)
+
+    def coding(self):
+        # This function need a student object!
+        if self.switch is True:
+            print("The student knowledge level increasing and the energy level decreasing.")
+        else:
+            print("The laptop isn't turned on.")
+        # This will be returned student object.
+        return
